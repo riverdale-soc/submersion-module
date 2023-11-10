0d7da1c8bccbe3516f85ff670a59456c8dfea8bb
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2023-11-09 22:50:03 -0500

    pushed touch sensor stuff to own component

--- main/submersion_module.c
+++ main/submersion_module.c
@@ -48,11 +48,11 @@
 #include "submersion_module.h"
 #include "gps_parser.h"
 #include "nmea_parser.h"
-#include "driver/touch_pad.h"
+#include "touch_sensor.h"
 
 // Allocate GPIO32 - GPS Enable as an GPIO Output
 #define GPS_ENABLE 32
-#define TOUCH_THRESH_NO_USE 0
+
 
 // Base and end addresses of ULP coprocessor program binary blob
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
@@ -509,51 +509,6 @@ static void gps_power_on(void)
 static void gps_power_off(void)
 {
     gpio_set_level(GPS_ENABLE, 0);
-}
-
-static void calibrate_touch_pad(touch_pad_t pad)
-{
-    int avg = 0;
-    const size_t calibration_count = 128;
-    for (int i = 0; i < calibration_count; ++i) {
-        uint16_t val;
-        touch_pad_read(pad, &val);
-        avg += val;
-    }
-    avg /= calibration_count;
-    const int min_reading = 300;
-    if (avg < min_reading) {
-        printf("Touch pad #%d average reading is too low: %d (expecting at least %d). "
-               "Not using for deep sleep wakeup.\n", pad, avg, min_reading);
-        touch_pad_config(pad, 0);
-    } else {
-        int threshold = avg - 100;
-        printf("Touch pad #%d average: %d, wakeup threshold set to %d.\n", pad, avg, threshold);
-        touch_pad_config(pad, threshold);
-    }
-}
-
-void deep_sleep_register_touch_wakeup(void)
-{
-    // Initialize touch pad peripheral.
-    // The default fsm mode is software trigger mode.
-    ESP_ERROR_CHECK(touch_pad_init());
-    // If use touch pad wake up, should set touch sensor FSM mode at 'TOUCH_FSM_MODE_TIMER'.
-    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
-    // Set reference voltage for charging/discharging
-    // In this case, the high reference valtage will be 2.4V - 1V = 1.4V
-    // The low reference voltage will be 0.5
-    // The larger the range, the larger the pulse count value.
-    touch_pad_set_voltage(TOUCH_HVOLT_2V4, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-    //init RTC IO and mode for touch pad.
-    touch_pad_config(TOUCH_PAD_NUM8, TOUCH_THRESH_NO_USE);
-    touch_pad_config(TOUCH_PAD_NUM9, TOUCH_THRESH_NO_USE);
-    calibrate_touch_pad(TOUCH_PAD_NUM8);
-    calibrate_touch_pad(TOUCH_PAD_NUM9);
-
-    printf("Enabling touch pad wakeup\n");
-    ESP_ERROR_CHECK(esp_sleep_enable_touchpad_wakeup());
-    ESP_ERROR_CHECK(esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON));
 }
 
 
