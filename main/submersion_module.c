0abaf8b2eb7471e6e74ac51a5ca39531e9239855
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2023-11-27 14:14:35 -0500

    added support for GPS event handler

--- main/submersion_module.c
+++ main/submersion_module.c
@@ -55,6 +55,49 @@ static const int ext_wakeup_pin_0 = 25;
 
 static const char *TAG = "Submersion Module";
 
+/**
+ * @brief GPS Event Handler
+ *
+ * @param event_handler_arg handler specific arguments
+ * @param event_base event base, here is fixed to ESP_NMEA_EVENT
+ * @param event_id event id
+ * @param event_data event specific arguments
+ */
+static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    gps_t *gps = NULL;
+    switch (event_id) {
+    case GPS_UPDATE:
+        gps = (gps_t *)event_data;
+        /* print information parsed from GPS statements */
+        ESP_LOGI(TAG, "%d/%d/%d %d:%d:%d => \r\n"
+                 "\t\t\t\t\t\tlatitude   = %.05f°N\r\n"
+                 "\t\t\t\t\t\tlongitude = %.05f°E\r\n"
+                 "\t\t\t\t\t\taltitude   = %.02fm\r\n"
+                 "\t\t\t\t\t\tspeed      = %fm/s",
+                 gps->date.year + YEAR_BASE, gps->date.month, gps->date.day,
+                 gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
+                 gps->latitude, gps->longitude, gps->altitude, gps->speed);
+        break;
+    case GPS_UNKNOWN:
+        /* print unknown statements */
+        ESP_LOGW(TAG, "Unknown statement:%s", (char *)event_data);
+        break;
+    default:
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 static void config_ext0_wakeup(void)
 {
     ESP_ERROR_CHECK(esp_sleep_enable_ext0_wakeup(ext_wakeup_pin_0, 1));
@@ -91,10 +134,15 @@ void app_main(void)
         ESP_ERROR_CHECK( ret );
 
     } else {
-        printf("Submersion Wake Up\n");
+
         // Turn on NEO-7M GPS module gated power supply
         gps_enable_init();
         gps_power_on();
+        /* NMEA parser configuration */
+        nmea_parser_config_t config = NMEA_PARSER_CONFIG_DEFAULT();
+        /* init NMEA parser library */
+        nmea_parser_handle_t nmea_hdl = nmea_parser_init(&config);
+        nmea_parser_add_handler(nmea_hdl, nmea_parser_gps_handler, NULL);
         // Start WiFi before using ESP-NOW
         wifi_init();
         // Initialize ESP-NOW and begin task
