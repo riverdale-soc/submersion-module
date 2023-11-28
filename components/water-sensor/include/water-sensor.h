0a67c1759d17d016eab67cb58120fe10360bd2bf
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2023-11-28 07:47:14 -0500

    update default pin confi

--- components/water-sensor/include/water-sensor.h
+++
@@ -1,8 +0,0 @@
-#ifndef WATER_SENSOR_H_
-#define WATER_SENSOR_H_
-
-static bool adc_calibration_init(adc_unit_t unit, adc_atten_t atten, adc_cali_handle_t *out_handle);
-static void adc_calibration_deinit(adc_cali_handle_t handle);
-void adc_read_oneshot(void);
-
-#endif // WATER_SENSOR_H_
